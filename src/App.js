93e49d162f1d1565d5f55eb60d737afba5a870a0
Waleed0260 <95635857+Waleed0260@users.noreply.github.com>
2023-02-28 13:03:38 -0800

    extension

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const data = [
+    {
+      name: "Search engines",
+      url:["https://www.google.com/", "https://www.bing.com/"]
+
+    }
+  ]
+
+  const[lists, setLists] = useState(data)
+
+
+  const openTabs = (url)=>{
+    for(const link of url ){
+      window.open(link, "blank")
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Coose your app 
+      <div className='list'>
+        {lists && lists.map((item)=>{
+          return(
+            <button className='buttons' onClick={()=> {openTabs(item.url)}}>{item.name}</button>
+
+          )
+        })}
+      </div>
     </div>
   );
 }
